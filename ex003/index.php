570a7767c5c70a967a23ee408330fd457de1e420
Jean-Dresbach <jean.rafael.dresbach@gmail.com>
2024-11-03 23:28:37 -0300

    meio da aula 20

---
+++ ex003/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+    //$nome = "Jean"; // Variavel
+    //$sobrenome = "Rafael"; // Variavel
+    const PAIS = "Brazil"; // Constante
+
+    $nome = "João"; // Variavel
+    $sobrenome = "Vitor"; // Variavel
+
+    echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+    ?>
+</body>
+
+</html>
